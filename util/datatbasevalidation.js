6aad3d2ac63d73e633253823dfc948703bc44868
vanessa082 <wahvanessa22@gmail.com>
2024-06-15 13:28:14 +0100

    writen the validator function to validate

--- util/datatbasevalidation.js
+++ util/datatbasevalidation.js
@@ -1,5 +1,16 @@
 function validateFmilydata(data) {
-  const { name, age, email, job, salary, residence  } = {
-    
+  if (typeof data !== 'object' || data !== null) {
+    throw  new Error ('data must be an object')
   }
+
+  const ecpectedKeys = ["name", "age", "email", "job"," salary", 'residence' ]
+
+  const dataKeys = Object.keys(data)
+
+  const misssingKeys = ecpectedKeys.filter((key) => !dataKeys.includes((key) => {
+    
+  })) 
+
+  const { name, age, email, job, salary, residence  } = data
+
 }
